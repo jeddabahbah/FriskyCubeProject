d58019051dc39f280c6909251a59fd16cf4addfb
Jessada Preprasat <Jessada Preprasat>
2016-09-22 03:46:18 +0700

    Call file from components

--- index.android.js
+++ index.android.js
@@ -2,6 +2,7 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  * @flow
+ *Android System
  */
 
 import React, { Component } from 'react';
@@ -11,43 +12,17 @@ import {
   Text,
   View
 } from 'react-native';
+import App from './components/app'
 
 class FriskyCubeProject extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+
+      <App/>
+
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('FriskyCubeProject', () => FriskyCubeProject);
